053fa36fdd5135a42ac368443e7297287aeac208
Yazodhara <yazodhara@hotmail.com>
2025-07-31 20:19:40 +0000

    Deploying to gh-pages from @ Yazodhara/yazodhara.github.io@e4788bc64a28c52397bef1f827bb633a0a99aaa4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "post-experiência-do-primeiro-dia",
         
           title: "Experiência do primeiro dia",
