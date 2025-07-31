994c99e29f06104ae83a05b68aafc18f7b14ba31
Yazodhara <yazodhara@hotmail.com>
2025-07-31 20:03:55 +0000

    Deploying to gh-pages from @ Yazodhara/yazodhara.github.io@a110f0d60ccdc700ab5d336374390d49bdf5511e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,20 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/al-folio/repositories/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/al-folio/teaching/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/al-folio/people/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
